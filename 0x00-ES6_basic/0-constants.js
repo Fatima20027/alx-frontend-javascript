a4cbd44b6fdf098fc1b211ba14786e99243dac19
Fatima20027 <fatima.hailou@icloud.com>
2024-09-30 16:39:56 +0100

    task 0

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,17 @@
+function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+function getLast() {
+  return ' is okay';
+}
+
+function taskNext() {
+  let componation = 'But sometimes let';
+  componation += getLast();
+
+  return componation;
+}
+
+console.log(`${taskFirst()} ${taskNext()}`);
