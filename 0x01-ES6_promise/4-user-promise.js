3742be2aee9659417c8c5bc05fb397f465cff382
Fatima20027 <fatima.hailou@icloud.com>
2024-10-02 18:12:06 +0100

    task 4

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,11 +1,3 @@
 export default function signUpUser(firstName, lastName) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (firstName && lastName) {
-        resolve(`${firstName} ${lastName}`);
-      } else {
-        reject();
-      }
-    });
-  });
+  return Promise.resolve({ firstName, lastName });
 }
