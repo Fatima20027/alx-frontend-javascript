879b01a7a0c258a4b119a5c7df22c4c40e25ce35
Fatima20027 <fatima.hailou@icloud.com>
2024-10-02 20:13:14 +0100

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUP = signUpUser(firstName, lastName);
+  const signUp = signUpUser(firstName, lastName);
   const upLoad = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUP, upLoad])
+  return Promise.allSettled([signUp, upLoad])
     .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
+      value: result.status === 'fulfilled' ? result.value : String(result.reason),
 
     })));
 }
