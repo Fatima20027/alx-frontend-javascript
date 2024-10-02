04742f058e8067e6de4d69aa8e617ad06468c2f2
Fatima20027 <fatima.hailou@icloud.com>
2024-10-02 16:09:38 +0100

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto, createUser])
-    .then((result) => { console.log(`${result[0].body} ${result[0].firstName} ${result[1].lastname}`); })
+    .then((result) => { console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastname}`); })
     .catch(() => console.log('Signup system offline'));
 }
