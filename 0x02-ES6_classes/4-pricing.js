8e1e54ccc1bc4624f37b9959a70a7ac2a301edad
Fatima20027 <fatima.hailou@icloud.com>
2024-10-04 18:25:15 +0100

    task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,43 @@
+import Currency from './3-currency';
+
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw TypeError('Currency must be a currency');
+    }
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} ${this._currency.code}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (amount !== 'number' && conversionRate !== 'number') {
+      throw TypeError('');
+    }
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
