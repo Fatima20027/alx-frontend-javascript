1691114b5e12cb39133db9348fdf382e7ee89d03
Fatima20027 <fatima.hailou@icloud.com>
2024-10-07 20:59:13 +0100

    task 3

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,13 +1,4 @@
-export default function getListStudents() {
-  const arr = [{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-
-  return arr;
-}
-
-export function getListStudentIds(arr) {
+export default function getListStudentIds(arr) {
   if (!Array.isArray(arr)) {
     return [];
   }
--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,16 +1,3 @@
-export default function getListStudents() {
-  const arr = [{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-
-  return arr;
-}
-
-export function getStudentsByLocation(arr, city) {
+export default function getStudentsByLocation(arr, city) {
   return arr.filter((students) => students.location === city);
 }
-
-const students = getListStudents();
-
-console.log(getStudentsByLocation(students, 'San Francisco'));
