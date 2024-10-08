e1b8c0cd6dc50b165619c24958aa92c8c7814f29
Fatima20027 <fatima.hailou@icloud.com>
2024-10-08 20:44:42 +0100

    task5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,7 @@ export default function createInt8TypedArray(length, position, value) {
   const dataView = new DataView(buffer);
 
   dataView.setInt8(position, value);
-  if (position >= length) {
+  if (position <= 0 || position >= length) {
     return new Error('Position outside range');
   }
   return dataView;
