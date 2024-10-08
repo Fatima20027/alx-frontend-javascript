1dbf78bd808644ce12eef75358eda82f4a342887
Fatima20027 <fatima.hailou@icloud.com>
2024-10-08 19:52:01 +0100

    task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,19 @@
 export default function updateStudentGradeByCity(arr, city, newGrades) {
   const arrFilter = arr.filter((student) => student.location === city);
-  const arrMap = arrFilter.map((student) => {
-    for (const studentGrade of newGrades) {
-      if (student.id === studentGrade.studentId) {
-        student.grade = studentGrade.grade;
+
+  const arrMap = arrFilter.map(
+    (student) => {
+      for (const studentGrade of newGrades) {
+        if (student.id === studentGrade.studentId) {
+          student.grade = studentGrade.grade; // eslint-disable-line no-param-reassign
+        }
       }
-    }
-    if (student.grade === undefined) {
-      student.grade = 'N/A';
-    }
-    return student;
-  });
+      if (student.grade === undefined) {
+        student.grade = 'N/A'; // eslint-disable-line no-param-reassign
+      }
+      return student;
+    },
+  );
 
   return arrMap;
 }
